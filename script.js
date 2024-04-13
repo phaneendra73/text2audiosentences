d12bfbddead4456b56fc95bd778e2f8f580fba5b
phaneendra73 <phaneendra3377@gmail.com>
2024-04-13 16:35:55 +0530

    text to audio project basic html

---
+++ script.js
@@ -0,0 +1,40 @@
+const question = document.querySelector("#question");
+
+const button = document.querySelector("#Button");
+
+const audio = document.querySelector("#audioelement");
+
+let lstaudios = "";
+
+const key = "1Oo7UJDxqt";
+
+button.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  console.log(question.value);
+  const apiUrl = `https://voicecup.com/api?key=[1Oo7UJDxqt]&q=${question.value}`;
+  const proxiedUrl = proxyUrl + apiUrl;
+  const data = getdata(proxiedUrl);
+});
+
+async function getdata(proxiedUrl) {
+  await fetch(proxiedUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      for (let index = 1; index < data.hits.length; index++) {
+        lstaudios += `<audio controls="true" preload="auto" controlsList="nodownload" >
+<source src="https://voicecup.com/play?key=${key}&filename=${data.hits[index].filename_audio}&filetype=mp4&start=${data.hits[index].start}&duration=${data.hits[index].duration}&subs_id=${data.hits[index].id}" type="audio/mp4">
+</audio><br>`;
+      }
+      audio.innerHTML = lstaudios;
+      console.log(lstaudios);
+    })
+
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
+}
